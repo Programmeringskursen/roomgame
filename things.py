d2604a91602ef06a9e2a5a457fef608952540cde
Murneh <jonas.johannesson90@gmail.com>
2015-04-02 22:42:07 +0200

    added __str__ and __repr__

--- things.py
+++ things.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import character
+import house
+import room
 class Things(object):
     
     def __init__(self, name, smell, position):
@@ -7,4 +10,11 @@ class Things(object):
         self.name = name
 	position.content.append(self)
 
+    def stink(self):
+	if character.
 
+    def __str__(self):
+	return self.name
+
+    def __repr__(self):
+	return self.name
