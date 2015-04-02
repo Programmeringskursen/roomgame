d16ebc325ce4b6daf1a698e85c499e6dbd78214d
Elin <elin_janeback@msn.com>
2015-04-02 17:50:07 +0200

    things function

---
+++ things.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class Things(object):
+    
+    def __init__(self, name, smell, position):
+        self.content = []
+        self.name = name
+	position.content.append(self)
+
+
